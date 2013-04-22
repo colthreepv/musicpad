0e71c72958dfdb9b8a03fa1d6f375acbfc739c35
Valerio <mrgamer@i530x.(none)>
2013-04-22 02:59:43 +0200

    soundcloud v1.1, now writes to file! AWSUM SPEEDZ!
    included nasty txt file ^_^

--- middlewares/soundcloud.js
+++ middlewares/soundcloud.js
@@ -1,5 +1,6 @@
 var util = require('util')
   , http = require('http')
+  , fs = require('fs')
   // Variables
   , client_id = 'b45b1aa10f1ac2941910a7f0d10f8e28'
   // Utilities
@@ -7,6 +8,13 @@ var util = require('util')
 
 exports.getsound = null;
 
+
+/**
+ * Function Re-Work
+ * IF song is not present inside REDIS cache, send a reply like res.send(200, 'downloadstarted');
+ * Otherwise send something like res.send(200, 'cachehit');
+ * So the backend can start polling for infos / use websocket in creative way to get infos about the download taking part.
+ */
 exports.getsound = function (req, res, next) {
   // If no param is passed, nothing to get! - TODO: change 403
   if (!req.param('url')) res.send(403);
@@ -14,27 +22,43 @@ exports.getsound = function (req, res, next) {
   var scReq = http.request({
     hostname: 'api.soundcloud.com',
     path: '/resolve?client_id='+client_id+'&url='+encodeURIComponent(req.param('url'))
-  }, function (response) {
+  }, function (scResponse) {
     var songID;
 
     // if the response is a redirect, that's what we want
-    if (response.statusCode === 302) {
-      songID = response.headers.location.match(/tracks\/(\d+)/)[1];
+    if (scResponse.statusCode === 302) {
+      songID = scResponse.headers.location.match(/tracks\/(\d+)/)[1];
+      // TODO - Check on REDIS if the file it has been already downloaded.
+      // IF NOT, go on.
+      // ELSE, send back to res the cached file!
 
       // nested Request to SoundCloud API
       var songURL = http.request({
         hostname: 'api.soundcloud.com',
         path: '/i1/tracks/'+songID+'/streams?client_id='+client_id
-      }, function (songResponse) {
+      }, function (songURLResponse) {
         var dataToParse = '';
-        songResponse.setEncoding('utf8');
-        songResponse.on('data', function (chunk) {
+        songURLResponse.setEncoding('utf8');
+        songURLResponse.on('data', function (chunk) {
           dataToParse += chunk;
         });
-        // when songResponse *FINALLY* ends, we have the responseURL!
-        songResponse.on('end', function() {
+        // when songURLResponse *FINALLY* ends, we have the responseURL!
+        songURLResponse.on('end', function() {
           dataToParse = JSON.parse(dataToParse);
-          res.send(dataToParse.http_mp3_128_url);
+          // res.send(dataToParse.http_mp3_128_url);
+          // So we now write the file to disk!
+          var songDownload = http.request({
+            hostname: dataToParse.http_mp3_128_url.match(/:\/\/(.*soundcloud.com)(.*)/)[1],
+            path: dataToParse.http_mp3_128_url.match(/:\/\/(.*soundcloud.com)(.*)/)[2]
+          },function (songDWResponse) {
+            var SongWriter = fs.createWriteStream(songID+'.mp3');
+            songDWResponse.pipe(SongWriter);
+            songDWResponse.on('end', function() {
+              res.send(200, 'download complete!');
+            });
+          });
+          // Close request, don't ever forget.
+          songDownload.end();
         });
       });
       // Close request, don't ever forget.
