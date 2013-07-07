a8041442b2f8bc579fa880b1479d530d12e298d1
mrgamer <mister.gamer@gmail.com>
2013-07-07 09:06:29 +0200

    youtube function gave me headaches, commented withAudioCodec('copy') DONT ASK ME WHY IT CAUSES TROUBLES ....

--- middlewares/youtube.js
+++ middlewares/youtube.js
@@ -24,29 +24,29 @@ module.exports = function (ytID, statusCallback, doneCallback) {
     title = info.title;
     declaredFileLength = parseInt(format.size, 10);
     hq = (format.audioBitrate > 128) ? true : false;
-    statusCallback({ id: ytID, status: 'starting', title: title, hq: hq });
+    statusCallback({ id: ytID, status: 'starting', title: title, hq: hq, type: 'yt' });
   });
   ytStream.on('error', doneCallback);
 
   ffmpegProc = ffmpeg({ source: ytStream, timeout: 600 })
     .withNoVideo(true)
-    .withAudioCodec('copy')
+    // .withAudioCodec('copy')
     .toFormat('ogg')
-    .saveToFile('assets/yt/'+ytID+'.ogg');
-  // var videoDump = require('fs').createWriteStream('assets/yt/'+ytID+'.mp4');
+    .saveToFile('assets/yt/' + ytID + '.ogg');
+  // var videoDump = require('fs').createWriteStream('assets/yt/' + ytID + '.mp4');
   // ytStream.pipe(videoDump);
 
   ytStream.on('data', function (chunk) {
     partialBytes += chunk.length;
     statusCallback({
       id: ytID,
-      status: 'downloading',
+      status: 'progress',
       totalBytes: declaredFileLength,
       partialBytes: partialBytes,
-      progress: Math.round((partialBytes / declaredFileLength)*100)
+      progress: Math.round((partialBytes / declaredFileLength) * 100)
     });
   });
-  ytStream.on('end', function() {
-    doneCallback(null, { id: ytID, status: 'complete', title: title, hq: hq });
+  ytStream.on('end', function () {
+    doneCallback(null, { id: ytID, status: 'complete', title: title, hq: hq, type: 'yt' });
   });
 };
