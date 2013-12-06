21eb1d3e95b802edef882bc4aad244641b2d5695
mrgamer <mister.gamer@gmail.com>
2013-12-06 18:49:40 +0100

    dev(frontend): develop frontend structure

---
+++ frontend/Gruntfile.js
@@ -0,0 +1,63 @@
+module.exports = function (grunt) {
+
+  // Load tasks
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.loadNpmTasks('grunt-concat-sourcemap');
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    // concat
+    concat_sourcemap: {
+      options: {
+        sourcesContent: true
+      },
+      app: {
+        src: [
+          'app/app.js'
+        ],
+        dest: 'app.js'
+      },
+      libs: {
+        src: [
+          'libs/angular/angular.js',
+        ],
+        dest: 'libs.js'
+      }
+    },
+    // copy
+    copy: {
+      index: {
+        src: 'index.html',
+        dest: 'build/index.html'
+      }
+    },
+    // mkdir
+    mkdir: {
+      build: {
+        options: {
+          create: ['build']
+        }
+      }
+    },
+    // connect
+    connect: {
+      serve: {
+        options: {
+          port: 8181,
+          base: '.',
+          hostname: '*',
+          debug: true,
+          keepalive: true
+        }
+      }
+    },
+    watch: {
+
+    }
+  });
+
+  grunt.registerTask('default', ['concat_sourcemap', 'connect']);
+  // grunt.registerTask('build', ['mkdir', 'copy', 'build']);
+};
