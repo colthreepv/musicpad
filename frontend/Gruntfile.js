df3ca1b978a60777d1add833edc59ecc571a3d2d
mrgamer <mister.gamer@gmail.com>
2013-12-09 18:38:24 +0100

    dev(deps&grunt): dependencies fixed

--- frontend/Gruntfile.js
+++ frontend/Gruntfile.js
@@ -3,6 +3,7 @@ module.exports = function (grunt) {
   // Load tasks
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-mkdir');
   grunt.loadNpmTasks('grunt-concat-sourcemap');
 
@@ -41,6 +42,13 @@ module.exports = function (grunt) {
         }
       }
     },
+    // clean
+    clean: {
+      app: ['app.js'],
+      libs: ['libs.js'],
+      templates: ['templates.js'],
+      bower: ['libs/*']
+    },
     // connect
     connect: {
       serve: {
@@ -58,6 +66,7 @@ module.exports = function (grunt) {
     }
   });
 
-  grunt.registerTask('default', ['concat_sourcemap', 'connect']);
+  grunt.registerTask('trash', ['clean:app', 'clean:libs', 'clean:templates']);
+  grunt.registerTask('default', ['trash', 'concat_sourcemap', 'connect']);
   // grunt.registerTask('build', ['mkdir', 'copy', 'build']);
 };
